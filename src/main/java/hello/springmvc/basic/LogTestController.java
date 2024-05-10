637521870011e66c765f684626800c0c1db4d200
BMDK9 <qnrakdlzm77@naver.com>
2024-05-10 17:52:48 +0900

    feat : 로깅 간단히 알아보기

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,27 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+
+//    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @GetMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        // trace > debug > info > warn > error
+        // 개발서버는 debug, 운영서버는 info
+        log.trace("trace log = {}", name);
+        log.debug("debug log = {}", name);
+        log.info("info log = {}", name);
+        log.warn("warn log = {}", name);
+        log.error("error log = {}", name);
+
+        return "ok";
+    }
+}
